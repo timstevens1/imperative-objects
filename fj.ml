bb4a2abd47e6b95f20f53069bfc8c294b0b843b0
timstevens1 <timstvns3@gmail.com>
2018-05-06 18:30:16 -0400

    make sure to do rm -r _build before committing

--- fj.ml
+++ fj.ml
@@ -90,18 +90,16 @@ type term =
 
 type tlist = term list
 
-type value = 1
-        | VObjectCreation of cname * value list
+type value = VObjectCreation of cname * value list
         
 type tenv = cname string_map
 
 type fldlist = (cname * fname)  list
 
-type ctor = ConstructorDecl of cname * fldlist
 
 type method_decl = MethodDecl of cname * mname * fldlist * term 
 
-type class_decl = ClassDecl of cname * cname * fldlist  * ctor * method_decl list
+type class_decl = ClassDecl of cname * cname * fldlist * method_decl list
 
 type class_env = class_decl list
 
@@ -115,19 +113,19 @@ let rec append l1 l2 =
 let rec class_search (cenv: class_env) (c : cname) : class_decl = match cenv with
         | [] -> raise SUBTYPE_ERROR
         | cd::ce -> begin match cd with
-                |ClassDecl(c1,c2,fl,ctor,ml) -> if c = c1 then cd else class_search ce c
+                |ClassDecl(c1,c2,fl,ml) -> if c = c1 then cd else class_search ce c
         end
 
 let rec super_look (cenv: class_env) (c0 : cname) : cname  = match class_search cenv c0 with
-        |ClassDecl(c1,c2,_, _ ,_) -> c2
+        |ClassDecl(c1,c2,_,_) -> c2
 
 let rec is_subtype (cenv : class_env) (c0 : cname) (csuper : cname) : bool = let c1 = super_look cenv c0 in
         if c1 = csuper then true else
-        if c1 = c0 then false else
+        if c1 = "object" then false else
         is_subtype cenv c1 csuper
 
 let rec field_loo (cenv: class_env) (c : cname) : fldlist = match class_search cenv c with
-            | ClassDecl(c1,c2,fl,_,_) -> if c = c2 then fl else append fl (field_loo cenv c2)
+            | ClassDecl(c1,c2,fl,_) -> if c2 = "object" then fl else append fl (field_loo cenv c2)
 
 let rec field_look (cenv: class_env) (c : cname) : fldlist = let fl = field_loo cenv c in
         let rec uniq (fl : fldlist) (f : cname * fname) : bool = match fl with
@@ -147,7 +145,7 @@ let rec meth_list_search (ml : method_decl list) (m : mname) : method_decl = mat
         end
 
 let rec meth_type_look (cenv: class_env) (c : cname) (m : mname) : (cname list) * cname = match class_search cenv c with
-         | ClassDecl(c1,c2,_,_,mlist) -> try 
+         | ClassDecl(c1,c2,_,mlist) -> try 
              match meth_list_search mlist m with
              | MethodDecl(c0,m0,f,t) -> let rec fst_ext (fl : fldlist): fname list = begin match fl with
                                         | [] -> []
@@ -155,11 +153,11 @@ let rec meth_type_look (cenv: class_env) (c : cname) (m : mname) : (cname list) 
                                         end
                                         in
                                         ((fst_ext f),c0) 
-         with METHOD_ERROR -> if c1 = c2 then raise METHOD_ERROR else meth_type_look cenv c2 m
+         with METHOD_ERROR -> if c2 = "object" then raise METHOD_ERROR else meth_type_look cenv c2 m
 
 
 let rec meth_body_look (cenv: class_env) (c : cname) (m : mname) : (fname list) * term = match class_search cenv c with
-         | ClassDecl(c1,c2,_,_,mlist) -> try 
+         | ClassDecl(c1,c2,_,mlist) -> try 
              match meth_list_search mlist m with
              | MethodDecl(c0,m0,f,t) -> let rec snd_ext (fl : fldlist): fname list = begin match fl with
                                         | [] -> []
@@ -167,7 +165,7 @@ let rec meth_body_look (cenv: class_env) (c : cname) (m : mname) : (fname list) 
                                         end
                                         in
                                         ((snd_ext f),t) 
-         with METHOD_ERROR -> if c1 = c2 then raise METHOD_ERROR else meth_body_look cenv c2 m
+         with METHOD_ERROR -> if c2 = "object" then raise METHOD_ERROR else meth_body_look cenv c2 m
 
 let rec step (cenv : class_env) (e0 : term) : term = match e0 with
         | FldAccess(e1,f1) -> begin match e1 with
@@ -176,21 +174,22 @@ let rec step (cenv : class_env) (e0 : term) : term = match e0 with
                 end
         end
 
+
+let rec type_flds (fl : fldlist) (tl : tlist) (c: cname) : cname = match fl, tl with
+    | [], [] -> c
+    | [], _ -> raise TYPE_ERROR
+    | _, [] -> raise TYPE_ERROR
+    | f::fl1, t::tl1 -> if not(is_subtype cenv (fst f) (type_term cenv t)) then raise TYPE_ERROR else type_flds fl1 tl1
+
 let rec type_term (cenv : class_env) (e0 : term) : cname = begin match e0 with
         | FldAccess(e1,fl) -> let rec fld_find (flist : fldlist) (f: fname) : cname = begin match flist with
                                         | [] -> raise TYPE_ERROR
                                         | fd::fdl -> if (snd fd) = f then (fst fd) else fld_find fdl f
                                         end
                                         in fld_find (field_look cenv (type_term cenv e1)) fl
-        | MethodInvoke(e1,m,tl) -> raise TODO
-        | ObjectCreation(c,tl) ->
-                let rec type_flds (fl : fldlist) (tl : tlist) : cname = match fl, tl with
-                    | [], [] -> c
-                    | [], _ -> raise TYPE_ERROR
-                    | _, [] -> raise TYPE_ERROR
-                    | f::fl1, t::tl1 -> if not(is_subtype cenv (fst f) (type_term cenv t)) then raise TYPE_ERROR else type_flds fl1 tl1
-                in type_flds (field_look cenv c) tl 
-
+        | MethodInvoke(e1,m,tl) -> let mtl = meth_type_look cenv (type_term cenv e1) m in
+                type_flds (fst mtl) tl (snd mtl)
+        | ObjectCreation(c,tl) -> type_flds (field_look cenv c) tl c
         | Cast(c,t) -> c
         | Var(v) -> failwith "this one doesn't make sense"
         end
@@ -202,10 +201,7 @@ let rec type_meth (cenv : class_env) (cl : cname) (m : mname) : bool = let (ml, 
             | cn::cnl -> try (is_subtype cenv cn cn) && (type_list cenv cnl) with SUBTYPE_ERROR -> false
         in (type_list cenv ml) && is_subtype cenv rt (type_term cenv t)
         
-
-let rec type_cons (cenv : class_env) (cl : cname) : bool = raise TODO
-
-let rec type_class (cenv : class_env) (cl : cname) : bool = let ClassDecl(c0,c1,fl,ctor,ml) = class_search cenv cl in
+let rec type_class (cenv : class_env) (cl : cname) : bool = let ClassDecl(c0,c1,fl,ml) = class_search cenv cl in
     let rec meth_iter (cenv: class_env) (cl: cname) (ml: method_decl list) = match ml with
             | [] -> true
             | md::ml' -> match md with MethodDecl(_,mn,_,_) -> (type_meth cenv cl mn) && (meth_iter cenv cl ml')
