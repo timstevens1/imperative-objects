05e8dd70331593539c149ccf771e67f952a89194
timstevens1 <timstvns3@gmail.com>
2018-05-06 22:44:08 -0400

    passing test case

--- fj.ml
+++ fj.ml
@@ -1,85 +1,20 @@
 open Util
-open StringSetMap
 
-(* The Assignment:
- * 
- * Fill in the `raise TODO` parts of the code:
- * - 22 cases in the `step` function
- * - 07 cases in the `scope_ok` function
- * - 08 cases in the `infer` function
- *
- * See the writeup for the specification for `step`, `scope_ok` and `infer`
- * functions that you must implement.
- *
- * Passing all of the tests does not guarantee 100%. You may want to write some
- * tests of your own.
- *
- * The last case of `infer` is for unpack expressions. In both the writeup and
- * code we define an alternative (and equivalent) rule [Unpack-Alt] which is
- * more suggestive of an implementation than the original rule [Unpack].
- *
- * If you ever need to compare two types for equality, use the function tequal,
- * and not (=). tequal will compare types for alpha-equivalence, e.g., ∀X.X and
- * ∀Y.Y are alpha equivalent, so tequal (∀Y.Y) (∀X.X) will return true, whereas
- * (∀Y.Y) = (∀X.X) using OCaml's (=) operation will return false.
- *
- * You may find the following functions helpful:
- *
- * The empty string set: 
- *   
- *   on paper: {}
- *   in OCaml: StringSet.empty
- *
- * The singleton string set:
- *
- *   on paper: {x}
- *   in OCaml: StringSet.singleton x
- *   or      : StringSet.of_list [x]
- *
- * String set union:
- *
- *   on paper: S₁ ∪ S₂
- *   in OCaml: StringSet.union s1 s2
- *
- * Removing from a string set:
- *
- *   on paper: S \ {x}
- *   in OCaml: StringSet.remove x s
- *
- * Testing membership in a string set:
- *
- *   on paper: x ∈ S
- *   in OCaml: StringSet.mem x s
- *
- * Analogously for maps:
- *
- *   on paper: {}
- *   in OCaml: StringMap.empty
- *
- *   on paper: {x ↦ τ}
- *   in OCaml: StringMap.singleton x t
- *
- *   on paper: Γ[x ↦ τ]
- *   in OCaml: StringMap.add x t g
- *
- *   on paper Γ(x) = τ
- *   in OCaml: StringMap.find x g = t
- *
- * Remember to always use OCaml's structural equality (=), and never use
- * physical equality (==).
- *)
 exception TODO
-exception SUBTYPE_ERROR
 exception METHOD_ERROR
 exception TYPE_ERROR
-exception AMBIGUOUS_FIELDS_ERROR
-
+exception FIELD_ERROR
+exception CLASS_ERROR
 
 type cname = string
+[@@deriving show {with_path = false}]
 type fname = string
+[@@deriving show {with_path = false}]
 type mname = string
+[@@deriving show {with_path = false}]
 
 type var = string
+[@@deriving show {with_path = false}]
 
 type term =
         | Var of var
@@ -87,21 +22,28 @@ type term =
         | MethodInvoke of term * mname * term list
         | ObjectCreation of cname * term list
         | Cast of cname * term
+[@@deriving show {with_path = false}]
+
 
 type tlist = term list
+[@@deriving show {with_path = false}]
 
 type value = VObjectCreation of cname * value list
+[@@deriving show {with_path = false}]
         
-type tenv = cname string_map
 
 type fldlist = (cname * fname)  list
+[@@deriving show {with_path = false}]
 
 
 type method_decl = MethodDecl of cname * mname * fldlist * term 
+[@@deriving show {with_path = false}]
 
 type class_decl = ClassDecl of cname * cname * fldlist * method_decl list
+[@@deriving show {with_path = false}]
 
 type class_env = class_decl list
+[@@deriving show {with_path = false}]
 
 (* type tclass = *)
 
@@ -111,7 +53,7 @@ let rec append l1 l2 =
   | [] -> l2
 
 let rec class_search (cenv: class_env) (c : cname) : class_decl = match cenv with
-        | [] -> raise SUBTYPE_ERROR
+        | [] -> raise TYPE_ERROR
         | cd::ce -> begin match cd with
                 |ClassDecl(c1,c2,fl,ml) -> if c = c1 then cd else class_search ce c
         end
@@ -136,7 +78,7 @@ let rec field_look (cenv: class_env) (c : cname) : fldlist = let fl = field_loo 
             | [] -> true
             | f::fl1 -> (uniq fl f) && (is_uniq fl1)
         in 
-        if (is_uniq fl) then fl else raise AMBIGUOUS_FIELDS_ERROR 
+        if (is_uniq fl) then fl else raise FIELD_ERROR 
 
 let rec meth_list_search (ml : method_decl list) (m : mname) : method_decl = match ml with
         | [] -> raise METHOD_ERROR
@@ -166,20 +108,15 @@ let rec meth_body_look (cenv: class_env) (c : cname) (m : mname) : (fname list) 
                                         in
                                         ((snd_ext f),t) 
          with METHOD_ERROR -> if c2 = "object" then raise METHOD_ERROR else meth_body_look cenv c2 m
-
+(*
 let rec step (cenv : class_env) (e0 : term) : term = match e0 with
         | FldAccess(e1,f1) -> begin match e1 with
                 | VObjectCreation(c',vlist') -> begin match f1 with
                         | 
                 end
         end
+*)
 
-
-let rec type_flds (fl : fldlist) (tl : tlist) (c: cname) : cname = match fl, tl with
-    | [], [] -> c
-    | [], _ -> raise TYPE_ERROR
-    | _, [] -> raise TYPE_ERROR
-    | f::fl1, t::tl1 -> if not(is_subtype cenv (fst f) (type_term cenv t)) then raise TYPE_ERROR else type_flds fl1 tl1
 
 let rec type_term (cenv : class_env) (e0 : term) : cname = begin match e0 with
         | FldAccess(e1,fl) -> let rec fld_find (flist : fldlist) (f: fname) : cname = begin match flist with
@@ -187,9 +124,20 @@ let rec type_term (cenv : class_env) (e0 : term) : cname = begin match e0 with
                                         | fd::fdl -> if (snd fd) = f then (fst fd) else fld_find fdl f
                                         end
                                         in fld_find (field_look cenv (type_term cenv e1)) fl
-        | MethodInvoke(e1,m,tl) -> let mtl = meth_type_look cenv (type_term cenv e1) m in
-                type_flds (fst mtl) tl (snd mtl)
-        | ObjectCreation(c,tl) -> type_flds (field_look cenv c) tl c
+        | MethodInvoke(e1,m,tl) -> let rec type_flds (cenv: class_env) (ml : cname list) (tl : tlist) (c: cname) : cname = match ml, tl with
+            | [], [] -> c
+            | [], _ -> raise TYPE_ERROR
+            | _, [] -> raise TYPE_ERROR
+            | m::ml1, t::tl1 -> if not(is_subtype cenv m (type_term cenv t)) then raise TYPE_ERROR else (type_flds cenv ml1 tl1 c)
+
+                in let mtl = meth_type_look cenv (type_term cenv e1) m in
+                type_flds cenv (fst mtl) tl (snd mtl)
+        | ObjectCreation(c,tl) -> let rec type_flds (cenv: class_env) (fl : fldlist) (tl : tlist) (c: cname) : cname = match fl, tl with
+            | [], [] -> c
+            | [], _ -> raise TYPE_ERROR
+            | _, [] -> raise TYPE_ERROR
+            | f::fl1, t::tl1 -> if not(is_subtype cenv (fst f) (type_term cenv t)) then raise TYPE_ERROR else (type_flds cenv fl1 tl1 c)
+        in type_flds cenv (field_look cenv c) tl c
         | Cast(c,t) -> c
         | Var(v) -> failwith "this one doesn't make sense"
         end
@@ -198,7 +146,7 @@ let rec type_meth (cenv : class_env) (cl : cname) (m : mname) : bool = let (ml, 
         let (fs, t) = meth_body_look cenv cl m in
         let rec type_list (cenv : class_env) (clist : cname list) : bool = match clist with
             | [] -> true
-            | cn::cnl -> try (is_subtype cenv cn cn) && (type_list cenv cnl) with SUBTYPE_ERROR -> false
+            | cn::cnl -> try (is_subtype cenv cn cn) && (type_list cenv cnl) with TYPE_ERROR -> false
         in (type_list cenv ml) && is_subtype cenv rt (type_term cenv t)
         
 let rec type_class (cenv : class_env) (cl : cname) : bool = let ClassDecl(c0,c1,fl,ml) = class_search cenv cl in
@@ -207,3 +155,40 @@ let rec type_class (cenv : class_env) (cl : cname) : bool = let ClassDecl(c0,c1,
             | md::ml' -> match md with MethodDecl(_,mn,_,_) -> (type_meth cenv cl mn) && (meth_iter cenv cl ml')
     in meth_iter cenv cl ml
 
+
+let rec type_cenv (cenv : class_env) : bool = match cenv with
+        | [] -> true
+        | cd::cenv' -> match cd with ClassDecl(c0,_,_,_) -> type_class cenv c0 && type_cenv cenv'
+
+
+
+(* testing *)
+
+type 'a test_result =
+  | R of 'a
+  | TypeError
+  | FieldError
+  | MethodError
+  | ClassError
+[@@deriving show {with_path = false}]
+
+type test_pack = class_env * term
+[@@deriving show {with_path = false}]
+
+let type_testing (tp : test_pack) : cname test_result = let (cenv, t) = tp in 
+    try
+        if type_cenv cenv then R(type_term cenv t) else raise CLASS_ERROR
+    with
+    | TYPE_ERROR -> TypeError
+    | FIELD_ERROR -> FieldError
+    | METHOD_ERROR -> MethodError
+    | CLASS_ERROR -> ClassError
+
+let enviro : class_env = [ClassDecl("A","object",[("object","thing")],[MethodDecl("B","Nothing",[],ObjectCreation("B",[]))]); ClassDecl("B","object",[],[])]
+        
+let type_test_block : test_block = 
+    TestBlock("TYPING", [((enviro, FldAccess(ObjectCreation("A",[ObjectCreation("B",[])]),"thing")), R("B"))],type_testing,(=),[%show : test_pack],[%show: cname test_result])
+
+
+let _ = _SHOW_PASSED_TESTS := true ;
+run_tests [type_test_block]
